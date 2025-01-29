9c8a4787822a5b673a96fe4f3c71e5e4b458759a
matheusuztp <matheusuztp@hotmail.com>
2025-01-29 08:35:26 -0300

    Add simple interface for test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
@@ -7,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+//pagina inicial para testar a API
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/api', userRoutes);
 
